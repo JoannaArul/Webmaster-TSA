bc8aac7a9fed7cbc4f99343d88716b641aab77b7
Joanna Arul Jeeva <joannaaruljeeva@gmail.com>
2026-03-09 21:41:30 -0400

    Removed image optimizer

--- WebmasterTSA/vite.config.js
+++ WebmasterTSA/vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteImageOptimizer({
-      jpg: { quality: 80 },
-      png: { quality: 80 },
-      webp: { quality: 80 },
-    }),
-  ],
+  plugins: [react()],
 })
